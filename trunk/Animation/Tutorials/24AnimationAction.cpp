0de27ced44da953bf6175590a888fb4234777392
David Kabala <djkabala@gmail.com>
2009-07-16 17:40:55 +0000

    added animation action tutorial

---
+++ trunk/Animation/Tutorials/24AnimationAction.cpp
@@ -0,0 +1,488 @@
+// OpenSG Tutorial Example: Hello World
+//
+// Minimalistic OpenSG program
+// 
+// This is the shortest useful OpenSG program 
+// (if you remove all the comments ;)
+//
+// It shows how to use OpenSG together with GLUT to create a little
+// interactive scene viewer.
+//
+
+// General OpenSG configuration, needed everywhere
+#include <OpenSG/OSGConfig.h>
+
+// Methods to create simple geometry: boxes, spheres, tori etc.
+#include <OpenSG/OSGSimpleGeometry.h>
+
+// A little helper to simplify scene management and interaction
+#include <OpenSG/OSGSimpleSceneManager.h>
+
+#include <OpenSG/OSGSimpleMaterial.h>
+
+#include <OpenSG/OSGComponentTransform.h>
+#include <OpenSG/OSGTransform.h>
+#include <OpenSG/OSGTypeFactory.h>
+#include <OpenSG/OSGFieldFactory.h>
+
+#include <OpenSG/OSGFieldContainerFactory.h>
+#include <OpenSG/OSGSimpleAttachments.h>
+
+#include <OpenSG/Toolbox/OSGFieldContainerUtils.h>
+
+// Input
+#include <OpenSG/Input/OSGKeyListener.h>
+#include <OpenSG/Input/OSGWindowAdapter.h>
+#include <OpenSG/Input/OSGWindowUtils.h>
+
+//Animation
+#include <OpenSG/Animation/OSGKeyframeSequences.h>
+#include <OpenSG/Animation/OSGKeyframeAnimator.h>
+#include <OpenSG/Animation/OSGFieldAnimation.h>
+#include <OpenSG/Animation/OSGElapsedTimeAnimationAdvancer.h>
+
+// UserInterface Headers
+#include <OpenSG/UserInterface/OSGUIForeground.h>
+#include <OpenSG/UserInterface/OSGInternalWindow.h>
+#include <OpenSG/UserInterface/OSGUIDrawingSurface.h>
+#include <OpenSG/UserInterface/OSGGraphics2D.h>
+#include <OpenSG/UserInterface/OSGLookAndFeelManager.h>
+// 01 Button Headers
+#include <OpenSG/UserInterface/OSGButton.h>
+#include <OpenSG/UserInterface/OSGToggleButton.h>
+#include <OpenSG/UserInterface/OSGUIFont.h>
+#include <OpenSG/UserInterface/OSGColorLayer.h>
+#include <OpenSG/UserInterface/OSGFlowLayout.h>
+#include <OpenSG/UserInterface/OSGPolygonUIDrawObject.h>
+#include <OpenSG/UserInterface/OSGUIDrawObjectCanvas.h>
+
+
+// Activate the OpenSG namespace
+// This is not strictly necessary, you can also prefix all OpenSG symbols
+// with OSG::, but that would be a bit tedious for this example
+OSG_USING_NAMESPACE
+
+bool animated;
+// forward declaration so we can have the interesting stuff upfront
+void setupAnimation(void);
+void display(void);
+void reshape(Vec2f Size);
+
+class TutorialAnimationListener : public AnimationListener
+{
+public:
+   virtual void animationStarted(const AnimationEvent& e)
+   {
+   }
+
+   virtual void animationStopped(const AnimationEvent& e)
+   {
+   }
+
+   virtual void animationPaused(const AnimationEvent& e)
+   {
+   }
+
+   virtual void animationUnpaused(const AnimationEvent& e)
+   {
+   }
+
+   virtual void animationEnded(const AnimationEvent& e)
+   {
+   }
+
+   virtual void animationCycled(const AnimationEvent& e)
+   {
+       std::cout << "Animation Cycled.  Cycle Count: " << e.getAnimation()->getCycles() << std::endl;
+   }
+
+};
+
+// The SimpleSceneManager to manage simple applications
+SimpleSceneManager *mgr;
+
+Time TimeLastIdle;
+FieldAnimationPtr TheAnimation;
+TutorialAnimationListener TheAnimationListener;
+AnimationAdvancerPtr TheAnimationAdvancer;
+MaterialPtr TheTorusMaterial;
+
+KeyframeAnimatorPtr TheAnimator;
+KeyframeTransformationsSequencePtr TransformationKeyframes;
+KeyframeColorsSequencePtr ColorKeyframes;
+KeyframeVectorsSequencePtr VectorKeyframes;
+KeyframeRotationsSequencePtr RotationKeyframes;
+
+bool ExitApp = false;
+
+// Create a class to allow for the use of the Ctrl+q
+class TutorialKeyListener : public KeyListener
+{
+public:
+
+   virtual void keyPressed(const KeyEvent& e)
+   {
+       if(e.getKey() == KeyEvent::KEY_Q && e.getModifiers() & KeyEvent::KEY_MODIFIER_CONTROL)
+       {
+           ExitApp = true;
+       }
+
+       if(e.getKey() == KeyEvent::KEY_T)
+       {
+	 std::cout << "toggling" << std::endl;
+	 //TheAnimcationAdvancer->stop();
+	 
+       }
+   }
+
+   virtual void keyReleased(const KeyEvent& e)
+   {
+   }
+
+   virtual void keyTyped(const KeyEvent& e)
+   {
+   }
+};
+
+class TutorialWindowListener : public WindowAdapter
+{
+public:
+    virtual void windowClosing(const WindowEvent& e)
+    {
+        ExitApp = true;
+    }
+
+    virtual void windowClosed(const WindowEvent& e)
+    {
+        ExitApp = true;
+    }
+};
+
+class TutorialMouseListener : public MouseListener
+{
+  public:
+    virtual void mouseClicked(const MouseEvent& e)
+    {
+    }
+    virtual void mouseEntered(const MouseEvent& e)
+    {
+    }
+    virtual void mouseExited(const MouseEvent& e)
+    {
+    }
+    virtual void mousePressed(const MouseEvent& e)
+    {
+            mgr->mouseButtonPress(e.getButton(), e.getLocation().x(), e.getLocation().y());
+    }
+    virtual void mouseReleased(const MouseEvent& e)
+    {
+           mgr->mouseButtonRelease(e.getButton(), e.getLocation().x(), e.getLocation().y());
+    }
+};
+
+class TutorialMouseMotionListener : public MouseMotionListener
+{
+  public:
+    virtual void mouseMoved(const MouseEvent& e)
+    {
+            mgr->mouseMove(e.getLocation().x(), e.getLocation().y());
+    }
+
+    virtual void mouseDragged(const MouseEvent& e)
+    {
+            mgr->mouseMove(e.getLocation().x(), e.getLocation().y());
+    }
+};
+
+class TutorialUpdateListener : public UpdateListener
+{
+  public:
+    virtual void update(const UpdateEvent& e)
+    {
+		ElapsedTimeAnimationAdvancer::Ptr::dcast(TheAnimationAdvancer)->update(e.getElapsedTime());
+		if (animated)
+		 TheAnimation->update(TheAnimationAdvancer);
+    }
+};
+
+class ExampleButtonActionListener : public ActionListener
+{
+public:
+
+   virtual void actionPerformed(const ActionEvent& e)
+    {
+        std::cout << "Button 1 Action" << std::endl;
+	animated ^= 1;
+    }
+};
+
+
+// Initialize GLUT & OpenSG and set up the scene
+int main(int argc, char **argv)
+{
+    // OSG init
+    osgInit(argc,argv);
+
+    // Set up Window
+    WindowEventProducerPtr TutorialWindowEventProducer = createDefaultWindowEventProducer();
+    WindowPtr MainWindow = TutorialWindowEventProducer->initWindow();
+
+	beginEditCP(TutorialWindowEventProducer, WindowEventProducer::UseCallbackForDrawFieldMask | WindowEventProducer::UseCallbackForReshapeFieldMask);
+		TutorialWindowEventProducer->setUseCallbackForDraw(true);
+		TutorialWindowEventProducer->setUseCallbackForReshape(true);
+	endEditCP(TutorialWindowEventProducer, WindowEventProducer::UseCallbackForDrawFieldMask | WindowEventProducer::UseCallbackForReshapeFieldMask);
+
+    TutorialWindowEventProducer->setDisplayCallback(display);
+    TutorialWindowEventProducer->setReshapeCallback(reshape);
+
+    //Add Window Listener
+    TutorialWindowListener TheTutorialWindowListener;
+    TutorialWindowEventProducer->addWindowListener(&TheTutorialWindowListener);
+    TutorialKeyListener TheKeyListener;
+    TutorialWindowEventProducer->addKeyListener(&TheKeyListener);
+    TutorialMouseListener TheTutorialMouseListener;
+    TutorialMouseMotionListener TheTutorialMouseMotionListener;
+    TutorialWindowEventProducer->addMouseListener(&TheTutorialMouseListener);
+    TutorialWindowEventProducer->addMouseMotionListener(&TheTutorialMouseMotionListener);
+	TutorialUpdateListener TheTutorialUpdateListener;
+    TutorialWindowEventProducer->addUpdateListener(&TheTutorialUpdateListener);
+
+    // Create the SimpleSceneManager helper
+    mgr = new SimpleSceneManager;
+
+	
+    TutorialWindowEventProducer->openWindow(Pnt2f(0,0),
+                                        Vec2f(1280,1024),
+                                        "OpenSG 01Animation Window");
+
+    // Tell the Manager what to manage
+    mgr->setWindow(TutorialWindowEventProducer->getWindow());
+
+    ////////////////
+        // Create the Graphics
+    GraphicsPtr TutorialGraphics = osg::Graphics2D::create();
+
+    // Initialize the LookAndFeelManager to enable default settings
+    LookAndFeelManager::the()->getLookAndFeel()->init();
+    /******************************************************
+
+                 Create an Button Component and
+                 a simple Font.
+                 See 17Label_Font for more
+                 information about Fonts.
+
+    ******************************************************/
+    ButtonPtr ExampleButton = osg::Button::create();
+
+    UIFontPtr ExampleFont = osg::UIFont::create();
+
+        beginEditCP(ExampleButton, Button::MinSizeFieldMask | Button::MaxSizeFieldMask | Button::PreferredSizeFieldMask | Button::ToolTipTextFieldMask | Button::TextFieldMask |
+        Button::FontFieldMask | Button::TextColorFieldMask | Button::RolloverTextColorFieldMask | Button::ActiveTextColorFieldMask | Button::AlignmentFieldMask);
+            ExampleButton->setMinSize(Vec2f(50, 25));
+            ExampleButton->setMaxSize(Vec2f(200, 100));
+            ExampleButton->setPreferredSize(Vec2f(100, 50));
+            ExampleButton->setToolTipText("Button 1 ToolTip");
+
+            ExampleButton->setText("Button 1");
+            ExampleButton->setFont(ExampleFont);
+            ExampleButton->setTextColor(Color4f(1.0, 0.0, 0.0, 1.0));
+            ExampleButton->setRolloverTextColor(Color4f(1.0, 0.0, 1.0, 1.0));
+            ExampleButton->setActiveTextColor(Color4f(1.0, 0.0, 0.0, 1.0));
+            ExampleButton->setAlignment(Vec2f(.5,0.5));
+    endEditCP(ExampleButton, Button::MinSizeFieldMask | Button::MaxSizeFieldMask | Button::PreferredSizeFieldMask | Button::ToolTipTextFieldMask | Button::TextFieldMask |
+	      Button::FontFieldMask | Button::TextColorFieldMask | Button::RolloverTextColorFieldMask | Button::ActiveTextColorFieldMask | Button::AlignmentFieldMask);
+
+
+    // Create an ActionListener and assign it to ExampleButton
+    // This Class is defined above, and will cause the output
+    // window to display "Button 1 Action" when pressed
+    ExampleButtonActionListener TheExampleButtonActionListener;
+    ExampleButton->addActionListener(&TheExampleButtonActionListener);
+    
+    
+    //////////////////
+    //Torus Material
+    TheTorusMaterial = SimpleMaterial::create();
+    beginEditCP(TheTorusMaterial);
+        SimpleMaterialPtr::dcast(TheTorusMaterial)->setAmbient(Color3f(0.3,0.3,0.3));
+        SimpleMaterialPtr::dcast(TheTorusMaterial)->setDiffuse(Color3f(0.7,0.7,0.7));
+        SimpleMaterialPtr::dcast(TheTorusMaterial)->setSpecular(Color3f(1.0,1.0,1.0));
+    endEditCP(TheTorusMaterial);
+
+    //Torus Geometry
+    GeometryPtr TorusGeometry = makeTorusGeo(.5, 2, 32, 32);
+    beginEditCP(TorusGeometry);
+        TorusGeometry->setMaterial(TheTorusMaterial);
+    endEditCP  (TorusGeometry);
+    
+    NodePtr TorusGeometryNode = Node::create();
+    beginEditCP(TorusGeometryNode, Node::CoreFieldMask);
+        TorusGeometryNode->setCore(TorusGeometry);
+    endEditCP  (TorusGeometryNode, Node::CoreFieldMask);
+
+    //Make Torus Node
+    NodePtr TorusNode = Node::create();
+    TransformPtr TorusNodeTrans;
+    TorusNodeTrans = Transform::create();
+    setName(TorusNodeTrans, std::string("TorusNodeTransformationCore"));
+
+    beginEditCP(TorusNode, Node::CoreFieldMask | Node::ChildrenFieldMask);
+        TorusNode->setCore(TorusNodeTrans);
+        TorusNode->addChild(TorusGeometryNode);
+    endEditCP  (TorusNode, Node::CoreFieldMask | Node::ChildrenFieldMask);
+
+    //Make Main Scene Node
+    NodePtr scene = Node::create();
+    ComponentTransformPtr Trans;
+    Trans = ComponentTransform::create();
+    setName(Trans, std::string("MainTransformationCore"));
+    beginEditCP(scene, Node::CoreFieldMask | Node::ChildrenFieldMask);
+    {
+        scene->setCore(Trans);
+ 
+        // add the torus as a child
+        scene->addChild(TorusNode);
+    }
+    endEditCP  (scene, Node::CoreFieldMask | Node::ChildrenFieldMask);
+
+    setupAnimation();
+
+    
+    // Create Background to be used with the Main InternalWindow
+    ColorLayerPtr MainInternalWindowBackground = osg::ColorLayer::create();
+    beginEditCP(MainInternalWindowBackground, ColorLayer::ColorFieldMask);
+        MainInternalWindowBackground->setColor(Color4f(1.0,1.0,1.0,0.5));
+    endEditCP(MainInternalWindowBackground, ColorLayer::ColorFieldMask);
+    InternalWindowPtr MainInternalWindow = osg::InternalWindow::create();
+    LayoutPtr MainInternalWindowLayout = osg::FlowLayout::create();
+	beginEditCP(MainInternalWindow, InternalWindow::ChildrenFieldMask | InternalWindow::LayoutFieldMask | InternalWindow::BackgroundsFieldMask | InternalWindow::AlignmentInDrawingSurfaceFieldMask | InternalWindow::ScalingInDrawingSurfaceFieldMask | InternalWindow::DrawTitlebarFieldMask | InternalWindow::ResizableFieldMask);
+       MainInternalWindow->getChildren().push_back(ExampleButton);
+       MainInternalWindow->setLayout(MainInternalWindowLayout);
+       MainInternalWindow->setBackgrounds(MainInternalWindowBackground);
+	   MainInternalWindow->setAlignmentInDrawingSurface(Vec2f(0.5f,0.5f));
+	   MainInternalWindow->setScalingInDrawingSurface(Vec2f(0.5f,0.5f));
+	   MainInternalWindow->setDrawTitlebar(false);
+	   MainInternalWindow->setResizable(false);
+    endEditCP(MainInternalWindow, InternalWindow::ChildrenFieldMask | InternalWindow::LayoutFieldMask | InternalWindow::BackgroundsFieldMask | InternalWindow::AlignmentInDrawingSurfaceFieldMask | InternalWindow::ScalingInDrawingSurfaceFieldMask | InternalWindow::DrawTitlebarFieldMask | InternalWindow::ResizableFieldMask);
+
+    // Create the Drawing Surface
+    UIDrawingSurfacePtr TutorialDrawingSurface = UIDrawingSurface::create();
+    beginEditCP(TutorialDrawingSurface, UIDrawingSurface::GraphicsFieldMask | UIDrawingSurface::EventProducerFieldMask);
+        TutorialDrawingSurface->setGraphics(TutorialGraphics);
+        TutorialDrawingSurface->setEventProducer(TutorialWindowEventProducer);
+    endEditCP(TutorialDrawingSurface, UIDrawingSurface::GraphicsFieldMask | UIDrawingSurface::EventProducerFieldMask);
+    
+	TutorialDrawingSurface->openWindow(MainInternalWindow);
+	
+	// Create the UI Foreground Object
+    UIForegroundPtr TutorialUIForeground = osg::UIForeground::create();
+
+    beginEditCP(TutorialUIForeground, UIForeground::DrawingSurfaceFieldMask);
+        TutorialUIForeground->setDrawingSurface(TutorialDrawingSurface);
+    endEditCP(TutorialUIForeground, UIForeground::DrawingSurfaceFieldMask);
+
+    mgr->setRoot(scene);
+
+    // Add the UI Foreground Object to the Scene
+    ViewportPtr TutorialViewport = mgr->getWindow()->getPort(0);
+    beginEditCP(TutorialViewport, Viewport::ForegroundsFieldMask);
+        TutorialViewport->getForegrounds().push_back(TutorialUIForeground);
+    beginEditCP(TutorialViewport, Viewport::ForegroundsFieldMask);
+
+    // show the whole scene
+    mgr->showAll();
+
+    TheAnimation->start();
+    
+    while(!ExitApp)
+    {
+        TutorialWindowEventProducer->update();
+        TutorialWindowEventProducer->draw();
+    }
+
+    osgExit();
+
+    return 0;
+}
+
+// Redraw the window
+void display(void)
+{
+    mgr->redraw();
+}
+
+// React to size changes
+void reshape(Vec2f Size)
+{
+    mgr->resize(Size.x(), Size.y());
+}
+
+// react to keys
+void keyboard(unsigned char k, int x, int y)
+{
+
+}
+
+void setupAnimation(void)
+{
+    //Color Keyframe Sequence
+    ColorKeyframes = KeyframeColorsSequence3f::create();
+    ColorKeyframes->addKeyframe(Color4f(1.0f,0.0f,0.0f,1.0f),0.0f);
+    ColorKeyframes->addKeyframe(Color4f(0.0f,1.0f,0.0f,1.0f),2.0f);
+    ColorKeyframes->addKeyframe(Color4f(0.0f,0.0f,1.0f,1.0f),4.0f);
+    ColorKeyframes->addKeyframe(Color4f(1.0f,0.0f,0.0f,1.0f),6.0f);
+
+	//Vector Keyframe Sequence
+    VectorKeyframes = KeyframeVectorsSequence3f::create();
+    VectorKeyframes->addKeyframe(Vec3f(0.0f,0.0f,0.0f),0.0f);
+    VectorKeyframes->addKeyframe(Vec3f(0.0f,1.0f,0.0f),1.0f);
+    VectorKeyframes->addKeyframe(Vec3f(1.0f,1.0f,0.0f),2.0f);
+    VectorKeyframes->addKeyframe(Vec3f(1.0f,0.0f,0.0f),3.0f);
+    VectorKeyframes->addKeyframe(Vec3f(0.0f,0.0f,0.0f),4.0f);
+    
+	//Rotation Keyframe Sequence
+    RotationKeyframes = KeyframeRotationsSequenceQuat::create();
+    RotationKeyframes->addKeyframe(Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*0.0),0.0f);
+    RotationKeyframes->addKeyframe(Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*0.5),1.0f);
+    RotationKeyframes->addKeyframe(Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*1.0),2.0f);
+    RotationKeyframes->addKeyframe(Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*1.5),3.0f);
+    RotationKeyframes->addKeyframe(Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*0.0),4.0f);
+
+	//Transformation Keyframe Sequence
+    TransformationKeyframes = KeyframeTransformationsSequence44f::create();
+	Matrix TempMat;
+	TempMat.setTransform(Vec3f(0.0f,0.0f,0.0f), Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*0.0));
+    TransformationKeyframes->addKeyframe(TempMat,1.0f);
+	TempMat.setTransform(Vec3f(0.0f,1.0f,0.0f), Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*0.5));
+    TransformationKeyframes->addKeyframe(TempMat,2.0f);
+	TempMat.setTransform(Vec3f(1.0f,1.0f,0.0f), Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*1.0));
+    TransformationKeyframes->addKeyframe(TempMat,3.0f);
+	TempMat.setTransform(Vec3f(1.0f,0.0f,0.0f), Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*1.5));
+    TransformationKeyframes->addKeyframe(TempMat,4.0f);
+	TempMat.setTransform(Vec3f(0.0f,0.0f,0.0f), Quaternion(Vec3f(0.0f,1.0f,0.0f), 3.14159f*0.0));
+    TransformationKeyframes->addKeyframe(TempMat,5.0f);
+    
+    //Animator
+    TheAnimator = KeyframeAnimator::create();
+    beginEditCP(TheAnimator, KeyframeAnimator::KeyframeSequenceFieldMask);
+        TheAnimator->setKeyframeSequence(TransformationKeyframes);
+    endEditCP(TheAnimator, KeyframeAnimator::KeyframeSequenceFieldMask);
+    
+    //Animation
+    TheAnimation = FieldAnimation::create();
+    beginEditCP(TheAnimation);
+        TheAnimation->setAnimator(TheAnimator);
+        TheAnimation->setInterpolationType(LINEAR_INTERPOLATION);
+        TheAnimation->setCycling(-1);
+    endEditCP(TheAnimation);
+	TheAnimation->setAnimatedField(getFieldContainer("Transform",std::string("TorusNodeTransformationCore")), std::string("matrix"));
+
+    //Animation Listener
+    TheAnimation->addAnimationListener(&TheAnimationListener);
+
+    //Animation Advancer
+    TheAnimationAdvancer = ElapsedTimeAnimationAdvancer::create();
+    beginEditCP(TheAnimationAdvancer);
+    ElapsedTimeAnimationAdvancer::Ptr::dcast(TheAnimationAdvancer)->setStartTime( 0.0 );
+    beginEditCP(TheAnimationAdvancer);
+}
