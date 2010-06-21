d3deedc3b3e51244936ed8209cab7b80f4153703
Daniel Guilliams <danielg@.vrac.iastate.edu>
2010-06-21 14:48:28 -0500

    Added: CMake for OSGLuaToolbox include for Cg (required for Lua bindings to CgFXMaterial) Added: Lua Bindings for CgFXMaterial
    Signed-off-by: Daniel Guilliams <danielg@iastate.edu>

--- Source/Contrib/Lua/Swig Binding Definitions/OpenSGToolbox-SwigGen.bat
+++ Source/Contrib/Lua/Swig Binding Definitions/OpenSGToolbox-SwigGen.bat
@@ -1,14 +1,14 @@
 
 echo off
 :Location of swig.exe
-:set SWIG_EXE_PATH=C:\Documents and Settings\David\My Documents\Work\swigwin-1.3.40\swig.exe
-set SWIG_EXE_PATH=C:\Users\David\Documents\Work\swigwin-2.0.0\swig.exe
+:set SWIG_EXE_PATH=C:\Program Files (x86)\swigwin-2.0.0\swig.exe
+set SWIG_EXE_PATH=C:\Program Files (x86)\swigwin-2.0.0\swig.exe
 
 :Location of the swig interface file
-set INTERFACE_FILE_PATH=C:\Users\David\Documents\Work\OpenSG-2.X\Toolbox\OpenSGToolbox\Source\Contrib\Lua\Swig Binding Definitions\OSGToolbox.i
+set INTERFACE_FILE_PATH=C:\Users\danielg\Documents\OpenSGToolbox\Source\Contrib\Lua\Swig Binding Definitions\OSGToolbox.i
 
 :Location of the output file to generate
-set OUTPUT_FILE_PATH=C:\Users\David\Documents\Work\OpenSG-2.X\Toolbox\OpenSGToolbox\Source\Contrib\Lua\ToolboxBindings\src\Bindings\OSGToolbox_wrap.cpp
+set OUTPUT_FILE_PATH=C:\Users\danielg\Documents\OpenSGToolbox\Source\Contrib\Lua\ToolboxBindings\src\Bindings\OSGToolbox_wrap.cpp
 
 "%SWIG_EXE_PATH%" -c++ -lua -o "%OUTPUT_FILE_PATH%" "%INTERFACE_FILE_PATH%"
 
