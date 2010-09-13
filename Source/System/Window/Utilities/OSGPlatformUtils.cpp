28d889cfe6ef826a8a14e10e55f5a10bd8e3e07b
David Kabala <djkabala@gmail.com>
2010-09-13 18:46:14 -0500

    Added: Utilities for getting the platform specific user AppData directory, and a directory for writing temporary files.

---
+++ Source/System/Window/Utilities/OSGPlatformUtils.cpp
@@ -0,0 +1,91 @@
+/*---------------------------------------------------------------------------*\
+ *                        OpenSG ToolBox Toolbox                             *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                          Authors: David Kabala                            *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+#include "OSGPlatformUtils.h"
+
+#ifdef __APPLE__
+#include "Folders.h"
+#endif
+
+#ifdef WIN32
+#endif
+
+#ifdef __linux
+#endif
+
+OSG_BEGIN_NAMESPACE
+
+/********************** OS X **************************/
+#ifdef __APPLE__
+
+BoostPath getPlatformUserAppDataDir(void)
+{
+    return BoostPath("~/Library/Application Support");
+}
+
+BoostPath getPlatformTempDataDir(void)
+{
+    //Carbon
+    FSFindFolder();
+    return BoostPath("/tmp");
+}
+
+#endif
+
+/********************* Linux **************************/
+#ifdef __linux
+
+BoostPath getPlatformUserAppDataDir(void)
+{
+    return BoostPath("~/");
+}
+
+BoostPath getPlatformTempDataDir(void)
+{
+    return BoostPath("/tmp");
+}
+
+#endif
+
+/******************** Windows *************************/
+#ifdef WIN32
+
+BoostPath getPlatformUserAppDataDir(void)
+{
+    //APPDATA
+    return BoostPath(getenv("APPDATA"));
+}
+
+BoostPath getPlatformTempDataDir(void)
+{
+    //TEMP
+    return BoostPath(getenv("TEMP"));
+}
+
+#endif
+
+OSG_END_NAMESPACE
+
---
+++ Source/System/Window/Utilities/OSGPlatformUtils.h
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------*\
+ *                        OpenSG ToolBox Toolbox                             *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                          Authors: David Kabala                            *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+#ifndef _OPENSG_PLATFORM_UTILS_H_
+#define _OPENSG_PLATFORM_UTILS_H_
+
+#include "OSGConfig.h"
+#include "OSGUtilDef.h"
+
+#include "OSGBoostPathFields.h"
+
+OSG_BEGIN_NAMESPACE
+
+BoostPath OSG_UTIL_DLLMAPPING getPlatformUserAppDataDir(void);
+
+BoostPath OSG_UTIL_DLLMAPPING getPlatformTempDataDir(void);
+
+OSG_END_NAMESPACE
+
+#endif
+
+
