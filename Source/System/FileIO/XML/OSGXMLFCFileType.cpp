43be761a7ea8930f1568db04120540d965e195be
David Kabala <djkabala@gmail.com>
2010-10-22 09:33:45 -0500

    Changed: Changed an error message in XMLFCFileType to give a more useful description.

--- Source/System/FileIO/XML/OSGXMLFCFileType.cpp
+++ Source/System/FileIO/XML/OSGXMLFCFileType.cpp
@@ -740,7 +740,8 @@ XMLFCFileType::IDLookupMap XMLFCFileType::createFieldContainers(rapidxml::node_i
             NewFCInfo._NewId = NewFCInfo._Ptr->getId();
             if(OldToNewFCLookupMap.insert(std::make_pair(OldFCId, NewFCInfo)).second == false)
             {
-                SFATAL << FileNameOrExtension << ": ERROR in XMLFCFileType::createFieldContainers()" <<
+                SFATAL << FileNameOrExtension << ": Could not create another FieldContainer with id: " << OldFCId
+                       << " because there is already a FieldContainer defined with that id."
                     std::endl;
             }
         }
